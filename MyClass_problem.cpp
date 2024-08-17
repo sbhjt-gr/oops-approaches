5fff02088f48be18dacbf72b2045b41ca881a076
Subhajit Gorai <47049949+sbhjt-gr@users.noreply.github.com>
2024-08-17 22:29:12 +0530

    Update MyClass_problem.cpp

--- MyClass_problem.cpp
+++ MyClass_problem.cpp
@@ -1,16 +1,11 @@
-/* Create a class MyClass which has member variable x of integer type and have the
-following constructors
+/* Create a class MyClass which has member variable x of integer type and have the following constructors
 MyClass( ) // Default
 MyClass(int)// Parameterized
 MyClass(MyClass &amp;) // Copy
-destructor ~ MyClass()
-member function to display value of x
+destructor ~ MyClass() member function to display value of x
 Create global objects, objects local to a function, local to main and object copy of another objects.
-Write suitable code to show how multiple no of objects are created and destroyed in
-the program. Display appropriate message which constructor is called using object creation.
-Write down observations the order the objects are created and destroyed. Modify program by using
-the concept of pointer to objects. Create dynamic object using new operator and use delete for
-destruction for the class Myclass.
+Write suitable code to show how multiple no of objects are created and destroyed in the program. Display appropriate message which constructor is called using object creation.
+Write down observations the order the objects are created and destroyed. Modify program by using the concept of pointer to objects. Create dynamic object using new operator and use delete fordestruction for the class Myclass.
 */
 
 #include <iostream>
