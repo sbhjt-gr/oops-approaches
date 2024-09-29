7526403a56f4c32835742bbb65f8a793ccbd2ff9
Subhajit Gorai <47049949+sbhjt-gr@users.noreply.github.com>
2024-09-29 19:37:47 +0530

    Create java-problem-1.java

---
+++ java-problem-1.java
@@ -0,0 +1,9 @@
+Create a class named “Student” having protected instance variables Roll_number, Student_Name and 
+class variable College_Name having values "HITK". Use constructor to initialize data members. It contains 
+method “showdata()” to display student's details. Inherit the Student class and create subclass named 
+“Exam ” having private instance variable marks of three subjects . Use constructor to initialize data 
+members .It contains the method “showresult()” which will display student information along with 
+average mark of three subjects. Write a program in java to test the “Exam” class and to display all the 
+details of Student.
+
+
