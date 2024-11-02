aff56ace8949c0b9af8a5776c59b3da7c9c5f092
Subhajit Gorai <sg_outlp@outlook.com>
2024-11-03 00:47:47 +0530

    Update to self-written code

--- MyClass_problem_c++.cpp
+++ MyClass_problem_c++.cpp
@@ -1,65 +1,77 @@
-/* Create a class MyClass which has member variable x of integer type and have the following constructors
-MyClass( ) // Default
-MyClass(int)// Parameterized
-MyClass(MyClass &amp;) // Copy
-destructor ~ MyClass() member function to display value of x
-Create global objects, objects local to a function, local to main and object copy of another objects.
-Write suitable code to show how multiple no of objects are created and destroyed in the program. Display appropriate message which constructor is called using object creation.
-Write down observations the order the objects are created and destroyed. Modify program by using the concept of pointer to objects. Create dynamic object using new operator and use delete fordestruction for the class Myclass.
+/* Create a class MyClass which has member variable x of integer type and have the following constructors:
+
+ MyClass( ) // Default
+ MyClass(int)// Parameterized
+ MyClass(MyClass &amp;) // Copy
+ Destructor ~ MyClass()
+ Member function to display value of x
+
+ Create global objects, objects local to a function, objects local to main and object copy of another objects.
+
+ Write suitable code to show how multiple no of objects are created and destroyed in the program. Display appropriate message which constructor is called using object creation.
+
+ Write down observations the order the objects are created and destroyed. 
+ 
+ Modify program by using the concept of pointer to objects. Create dynamic object using new operator and use delete for destruction for the class Myclass.
 */
 
 #include <iostream>
 
+using namespace std;
+
 class MyClass {
-public:
-    int x;
-
-    // Default constructor
-    MyClass() : x(0) {
-        std::cout << "Default constructor called. x = " << x << std::endl;
-    }
-
-    // Parameterized constructor
-    MyClass(int val) : x(val) {
-        std::cout << "Parameterized constructor called. x = " << x << std::endl;
-    }
-
-    // Copy constructor
-    MyClass(const MyClass& other) : x(other.x) {
-        std::cout << "Copy constructor called. x = " << x << std::endl;
-    }
-
-    // Destructor
-    ~MyClass() {
-        std::cout << "Destructor called. x = " << x << std::endl;
-    }
-
-    // Member function to display value of x
-    void display() const {
-        std::cout << "Value of x: " << x << std::endl;
-    }
+    private:
+        int x;
+    public:
+        MyClass() : x(0) {
+            cout << "Default constructor called." << endl;
+        }
+        MyClass(int val) : x(val) {
+            cout << "Parameterized constructor called." << endl;
+        }
+        MyClass(const MyClass &other) : x(other.x) {
+            cout << "Copy constructor called." << endl;
+        }
+        ~MyClass() {
+            cout << "Destructor called for MyClass." << endl;
+        }
+        void display() const {
+            cout << "Value of x: " << x << endl;
+        }
 };
 
-// Function to create a local object
-void createLocalObject() {
-    MyClass localObj(20);
-    localObj.display();
+MyClass globalObject(1);
+
+void functionWithLocalObject() {
+    cout << "Entering functionWithLocalObject()" << endl;
+    MyClass localToFunction(2);
+    localToFunction.display();
+    cout << "Exiting functionWithLocalObject()" << endl;
 }
 
 int main() {
-    static MyClass globalObj;
+    cout << "Entering main()" << endl;
+
+    cout << "Displaying global object:" << endl;
+    globalObject.display();
+
+    MyClass defaultObject;
+    defaultObject.display();
+
+    MyClass localToMain(3);
+    localToMain.display();
+
+    MyClass copyOfLocal(localToMain);
+    copyOfLocal.display();
+
+    functionWithLocalObject();
+
+    MyClass* dynamicObject = new MyClass(4);
+    dynamicObject->display();
+
+    delete dynamicObject;
     
-    MyClass mainObj(10);
-    mainObj.display();
-
-    createLocalObject();
-
-    MyClass copiedObj(mainObj);
-    copiedObj.display();
-
-    MyClass* dynamicObj = new MyClass(30);
-    dynamicObj->display();
-    delete dynamicObj; // Deleting the dynamically allocated object
+    cout << "Exiting main()" << endl;
 
     return 0;
 }
